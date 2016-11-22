516f94430c6c10b80d97f6b0a4ef881ba4b3fd3d
Jack <jack@atg.io>
2016-11-21 23:21:07 -0500

    Added incomplete beta and regularized beta functions (#25)

--- utils.go
+++ utils.go
@@ -3,6 +3,8 @@ package distributions
 import "math"
 
 const gamma_epsilon = 1e-14
+const beta_epsilon = 2.2204460492503131e-16
+const beta_iterations = 1e9
 
 // The  regularized lower incomplete gamma function.
 // Code kanged from SAMTools: https://github.com/lh3/samtools/blob/master/bcftools/kfunc.c
@@ -38,6 +40,7 @@ func Choose(n, k float64) float64 {
 }
 
 // A variadic version of the Beta function.
+// See: https://en.wikipedia.org/wiki/Beta_function
 func BetaFn(a ...float64) float64 {
   product := 1.0
   sum := 0.0
@@ -46,4 +49,57 @@ func BetaFn(a ...float64) float64 {
     sum += ai
   }
   return product / math.Gamma(sum)
+}
+
+// The incomplete beta function.
+// See: https://en.wikipedia.org/wiki/Beta_function#Incomplete_beta_function
+func BetaInc(a, b, x float64) float64 {
+  return RegBetaInc(a, b, x) * BetaFn(a, b)
+}
+
+// The regularized incomplete beta function.
+// See: https://en.wikipedia.org/wiki/Beta_function#Incomplete_beta_function
+func RegBetaInc(a, b, x float64) float64 {
+  if x == 0.0 {
+    return 0.0
+  }
+  if x == 1.0 {
+    return 1.0
+  }
+  lab, _ := math.Lgamma(a + b)
+  la, _ := math.Lgamma(a)
+  lb, _ := math.Lgamma(b)
+  lbeta := lab - la - lb + (a * math.Log(x)) + (b * math.Log(1-x))
+  if x < (a + 1) / (a + b + 2) {
+    return math.Exp(lbeta) * contFracBeta(a, b, x) / a
+  }
+  return 1 - math.Exp(lbeta) * contFracBeta(b, a, 1-x) / b
+}
+
+// Ref: https://malishoaib.wordpress.com/2014/04/15/the-beautiful-beta-functions-in-raw-python/
+func contFracBeta(a, b, x float64) float64 {
+  am, bm, az := 1.0, 1.0, 1.0
+  qab := a + b
+  qap := a + 1.0
+  qam := a - 1.0
+  bz := 1.0 - (qab * x / qap)
+  for i := 0.0; i <= beta_iterations; i += 1.0 {
+    em := i + 1.0
+    tem := em + em
+    d := em * (b - em) * x / ((qam + tem) * (a + tem))
+    ap := az + (d * am)
+    bp := bz + (d * bm)
+    d = -(a + em) * (qab + em) * x / ((qap + tem) * (a + tem))
+    app := ap + (d * az)
+    bpp := bp + (d * bz)
+    aold := az
+    am = ap / bpp
+    bm = bp / bpp
+    az = app / bpp
+    bz = 1.0
+    if math.Abs(az - aold) < beta_epsilon * math.Abs(az) {
+      return az
+    }
+  }
+  return math.NaN()
 }
--- utils_test.go
+++ utils_test.go
@@ -1,13 +1,14 @@
 package distributions
 
 import (
-  "math"  
+  "math"
   "testing"
 )
 
 type lowerIncGamma struct { s, x, out float64 }
 type nChoosek struct { n, k, out float64 }
-type betaFn struct { a, b, out float64 } 
+type betaFn struct { a, b, out float64 }
+type betaIncFn struct { x, a, b, out float64 }
 
 // Test at http://keisan.casio.com/exec/system/1180573447
 // Have to regularize it here.
@@ -59,6 +60,40 @@ func Test_BetaFn(t *testing.T) {
     result := BetaFn(example.a, example.b)
     if result != example.out {
       t.Fatalf("\n  Expected: %f\n  Got: %f\n", example.out, result)
+    }
+  }
+}
+
+func Test_BetaInc(t *testing.T) {
+  examples := []betaIncFn {
+    betaIncFn{ 0.8, 10,  2, 0.00292838679272727272727 },
+    betaIncFn{ 0.9, 14,  2, 0.00261449056628125714286 },
+    betaIncFn{ 0.6,  2,  4, 0.045648 },
+    betaIncFn{ 0.6,  1,  4, 0.2436 },
+    betaIncFn{ 0.5,  3,  5, 0.00736607142857142857143 },
+    betaIncFn{ 0.2,  1, 17, 0.057498941286067501176 },
+  }
+  for _, example := range examples {
+    result := BetaInc(example.a, example.b, example.x)
+    if !floatsPicoEqual(result, example.out) {
+      t.Fatalf("\n  Expected: %b\n  Got: %b\n", example.out, result)
+    }
+  }
+}
+
+func Test_RegBetaInc(t *testing.T) {
+  examples := []betaIncFn {
+    betaIncFn{ 0.8, 10,  2, 0.3221225472 },
+    betaIncFn{ 0.9, 14,  2, 0.549043018919064 },
+    betaIncFn{ 0.6,  2,  4, 0.91296 },
+    betaIncFn{ 0.6,  1,  4, 0.9744 },
+    betaIncFn{ 0.5,  3,  5, 0.7734375 },
+    betaIncFn{ 0.2,  1, 17, 0.97748200186314752 },
+  }
+  for _, example := range examples {
+    result := RegBetaInc(example.a, example.b, example.x)
+    if !floatsPicoEqual(result, example.out) {
+      t.Fatalf("\n  Expected: %b\n  Got: %b\n", example.out, result)
     }
   }
 }
